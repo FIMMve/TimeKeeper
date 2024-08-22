779ea58cdb0ad2b8f844487166afd6b352c1330e
Fabian Márquez <92053349+FIMMve@users.noreply.github.com>
2024-08-21 22:49:32 -0400

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,70 @@
+const themeSwitch = document.getElementById("theme-switch")
+
+themeSwitch.checked = localStorage.getItem("lightTheme") === "true";
+
+themeSwitch.addEventListener("change", e => {
+    e.currentTarget.checked === true
+        ? localStorage.setItem("lightTheme", "true")
+        : localStorage.removeItem("lightTheme")
+})
+
+const tempoRange = document.getElementById("tempo-range")
+const tempoValue = document.getElementById("tempo-value")
+const decreaseTempo = document.getElementById("decrease-tempo")
+const increaseTempo = document.getElementById("increase-tempo")
+
+const metronome = new Metronome()
+const playSwitch = document.getElementById("play-switch")
+
+playSwitch.addEventListener("click", () => {
+    metronome.startStop()
+})
+
+const divisions = document.querySelectorAll(".divisions")
+let divisionMultiplier = 1
+let activatedDivision = 1
+
+for(const division of divisions){
+    division.multiplier = divisionMultiplier
+    divisionMultiplier += 1
+
+    division.addEventListener("click", () => {
+        if(division.classList.contains("selected")){
+            division.classList.remove("selected")
+        }else{
+            for(const activeDivision of divisions){
+                activeDivision.classList.remove("selected")
+            }
+            division.classList.add("selected")
+            activatedDivision = division.multiplier
+
+            updateTempo()
+        }
+    })
+}
+
+tempoRange.value = localStorage.getItem("tempo")
+
+const updateTempo = () => {
+    tempoValue.textContent = tempoRange.value + " BPM"
+    metronome.tempo = tempoRange.valueAsNumber * activatedDivision
+    metronome.beats = 4 * activatedDivision
+
+    localStorage.setItem("tempo", tempoRange.value)
+}
+
+updateTempo()
+
+decreaseTempo.addEventListener("click", () => {
+    tempoRange.value = tempoRange.valueAsNumber - 5
+    updateTempo()
+})
+
+increaseTempo.addEventListener("click", () => {
+    tempoRange.value = tempoRange.valueAsNumber + 5
+    updateTempo()
+})
+
+tempoRange.addEventListener("input", () => {
+    updateTempo()
+})
---
+++ assets/js/metronome.js
@@ -0,0 +1,85 @@
+class Metronome{
+    constructor(tempo = 0, beats = 0){
+        this.audioContext = null
+        this.notesInQueue = []
+        this.currentBeatInBar = 0
+        this.beatsPerBar = beats
+        this.tempo = tempo
+        this.lookAhead = 25
+        this.scheduleAheadTime = 0.1
+        this.nextNoteTime = 0.0
+        this.isRunning = false
+        this.interval = null
+    }
+
+    nextNote(){
+        const secondsPerBeat = (60.0 / this.tempo)
+
+        this.nextNoteTime += secondsPerBeat
+        this.currentBeatInBar++
+
+        if(this.currentBeatInBar == this.beatsPerBar){
+            this.currentBeatInBar = 0
+        }
+    }
+
+    scheduleNote(beatNumber, time){
+        this.notesInQueue.push(
+            {
+                note: beatNumber,
+                time: time
+            }
+        )
+
+        const oscillator = this.audioContext.createOscillator()
+        const envelope = this.audioContext.createGain()
+
+        oscillator.frequency.value = 800
+
+        // TO DO: configure different frecuencys to each type of beat (first, secondary, divisions)
+
+        envelope.gain.value = 1
+        envelope.gain.exponentialRampToValueAtTime(1, time + 0.001)
+        envelope.gain.exponentialRampToValueAtTime(0.001, time + 0.02)
+
+        oscillator.connect(envelope)
+        envelope.connect(this.audioContext.destination)
+
+        oscillator.start(time)
+        oscillator.stop(time + 0.03)
+    }
+
+    scheduler(){
+        while (this.nextNoteTime < this.audioContext.currentTime + this.scheduleAheadTime ) {
+            this.scheduleNote(this.currentBeatInBar, this.nextNoteTime);
+            this.nextNote();
+        }
+    }
+
+    start(){
+        if(this.isRunning) return
+
+        if(this.audioContext == null){
+            this.audioContext = new (window.AudioContext || window.webkitAudioContext)()
+        }
+
+        this.isRunning = true
+        this.currentBeatInBar = 0
+        this.nextNoteTime = this.audioContext.currentTime + 0.05
+        this.interval = setInterval(() => this.scheduler(), this.lookAhead)
+    }
+
+    stop(){
+        this.isRunning = false
+
+        clearInterval(this.interval)
+    }
+
+    startStop(){
+        if(this.isRunning){
+            this.stop()
+        }else{
+            this.start()
+        }
+    }
+}
